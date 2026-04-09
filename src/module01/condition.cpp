73e813f66dad4c304547300c966f028c386c59ec
zhangsunjiankun <zhangsunjiankun@dingtalk.com>
2026-04-09 18:38:22 +0800

    删除conan配置

---
+++ src/module01/condition.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 大江流韵 on 2026/4/9.
+//
+#include <iostream>
+using namespace std;
+
+/**
+ * 条件语句
+ * 1. 跟Java基本上是一样的，有个别区别
+ */
+
+void demoIf() {
+    if (int x = 5) {
+        cout << "x is true" << endl;
+    }
+}
